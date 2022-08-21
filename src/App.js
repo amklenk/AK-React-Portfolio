506a1169fdffd39c3f324309cc078043b2fe605c
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-21 16:13:15 -0400

    create Header component and initial styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+// Why don't I need to import this?
 import './index.css';
 import About from './components/About';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <About/>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Header (){
+return(
+<div id="header">
+    <h2 class="nav-name">AMANDA KLENK</h2>
+    <nav class="nav-links">
+        <ul id="nav-ul">
+            <li class="nav-sections">
+                <a href="#about-section">About</a>
+            </li>
+            <li class="nav-sections">
+                <span>Skills</span>
+            </li>
+            <li class="nav-sections">
+                <span>Portfolio</span>
+            </li>
+            <li class="nav-sections">
+                <span>Contact</span>
+            </li>
+            <li class="nav-sections">
+                <span>Resume</span>
+            </li>
+        </ul>
+    </nav>
+</div>
+)
+}
+
+export default Header;
