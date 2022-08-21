a5ff267645aac2ba5beafae184e2364d4fa7f4ae
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-21 16:55:09 -0400

    render images and set up some styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
+// would eventually like to map over to render
+import emailIcon from '../../assets/icons/email-icon.png';
+import gitHub from '../../assets/icons/github-logo.png';
+import linkedIn from '../../assets/icons/linkedin-logo.png';
+import freeCode from '../../assets/icons/fCC-icon.png';
+
 
 function Footer(){
     return(
-        <footer>
-            <a id="Gmail-link" href="mailto: amandamklenk3@gmail.com" target="_blank" rel="noopener noreferrer" class="btn contact-details">
-                <img class="Gmail-icon" src="./assets/images/icons/email-icon.png" alt="Gmail icon"/>
+        <footer id='footer'>
+            <a className='contact-links' id="Gmail-link" href="mailto: amandamklenk3@gmail.com" target="_blank" rel="noopener noreferrer" class="btn contact-details">
+                <img class="Gmail-icon" src={emailIcon} alt="Gmail icon"/>
             </a>
-            <a id="LinkedIn-link" href="https://www.linkedin.com/in/amanda-klenk/" target="_blank" rel="noopener noreferrer" class="btn contact-details">
-                <img class="LinkedIn-icon" src="./assets/images/icons/linkedin-logo.png" alt="LinkedIn icon"/>
+            <a className='contact-links' id="LinkedIn-link" href="https://www.linkedin.com/in/amanda-klenk/" target="_blank" rel="noopener noreferrer" class="btn contact-details">
+                <img class="LinkedIn-icon" src={linkedIn} alt="LinkedIn icon"/>
             </a>
-            <a id="GitHub-link" href="https://github.com/amklenk" target="_blank" rel="noopener noreferrer" class="btn contact-details">
-                <img class="GitHub-icon" src="./assets/images/icons/github-logo.png" alt="GitHub logo"/>
+            <a className='contact-links' id="GitHub-link" href="https://github.com/amklenk" target="_blank" rel="noopener noreferrer" class="btn contact-details">
+                <img class="GitHub-icon" src={gitHub} alt="GitHub logo"/>
             </a>
-            <a id="FCC-link" href="https://www.freecodecamp.org/mklenk3" target="_blank" rel="noopener noreferrer" class="btn contact-details">
-                <img class="FCC-icon" src="./assets/images/icons/fCC-icon.png" alt="freeCodeCamp icon"/>
+            <a className='contact-links' id="FCC-link" href="https://www.freecodecamp.org/mklenk3" target="_blank" rel="noopener noreferrer" class="btn contact-details">
+                <img class="FCC-icon" src={freeCode} alt="freeCodeCamp icon"/>
             </a>
            <h4 class="footer-name">AMANDA KLENK</h4>
                 &copy; 2022
