38b7aecfffa8765fae2f02579f02ad5bde70d65f
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-23 15:03:27 -0400

    validate name and message

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,19 +8,28 @@ function Contact() {
     const { name, email, message } = formState;
     const [errorMessage, setErrorMessage] = useState('');
 
-    function handleChange(event){
+    function handleChange(event) {
         if(event.target.name === 'email'){
             const isValid = validateEmail(event.target.value);
-            if(!isValid) {
-                setErrorMessage('Your email is invalid');
+            // if it isn't valid, set the state of the error message to the following
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                // otherwise, keep error message blank
+                setErrorMessage('');
+            }
+        } else {
+            // for message and name, so that they cannot be blank
+            if (!event.target.value.length){
+                setErrorMessage(`${event.target.name} is required.`);
             } else {
                 setErrorMessage('');
             }
         }
-        if(!errorMessage) {
-            setFormState({ ...formState, [event.target.name]: event.target.value });
-        }
-    };
+        // use spread operator to retain the other key-value pairs in the object
+        if (!errorMessage){
+            setFormState({ ...formState, [event.target.name]: event.target.value });}
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -33,21 +42,24 @@ function Contact() {
     <Form className='mx-5' onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Name:</Form.Label>
-        <Form.Control type="name" placeholder="Name" defaultValue={name} onBlur={handleChange} />
+        <Form.Control type="name" placeholder="Name" name='Name' defaultValue={name} onBlur={handleChange} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" defaultValue={email} onBlur={handleChange} />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control type="email" name='Email' placeholder="Enter email" defaultValue={email} onBlur={handleChange} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Message:</Form.Label>
-        <Form.Control as="textarea" name="message" rows={5} defaultValue={message} onBlur={handleChange}/>
+        <Form.Control as="textarea" name="Message" rows={5} defaultValue={message} onBlur={handleChange}/>
       </Form.Group>
+
+      {errorMessage && (
+                <div>
+                    <p className='error-text'>{errorMessage}</p>
+                </div>
+              )}
 
       <Button variant="dark" type="submit">
         Submit
