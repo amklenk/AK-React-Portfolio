feb025efb8b6622456420d0d6559884a0fd2a470
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-24 13:26:31 -0400

    make change to import of index.css in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 // Why don't I need to import this?
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import About from './components/About';
