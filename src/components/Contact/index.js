2975e13e9517193ce1fc25b06d43db1b581f25f6
Amanda Klenk <amandamklenk3@gmail.com>
2022-08-25 12:03:38 -0400

    set up EmailJS in index.js of Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { validateEmail } from '../../utils/helpers';
+import emailjs from 'emailjs-com'
+// Do I need this?
+import { init } from 'emailjs-com';
+init(process.env.REACT_APP_USER_ID);
 
 function Contact() {
+  // setting change to validate
     const [formState, setFormState] = useState({ name:'', email:'', message:'' });
     const { name, email, message } = formState;
     const [errorMessage, setErrorMessage] = useState('');
-
+  // function to validate
     function handleChange(event) {
         if(event.target.name === 'Email'){
             const isValid = validateEmail(event.target.value);
@@ -30,11 +35,42 @@ function Contact() {
         if (!errorMessage){
             setFormState({ ...formState, [event.target.name]: event.target.value });}
     }
-
+  // funtion to submit
     function handleSubmit(event) {
         event.preventDefault();
         console.log(formState);
       };
+
+  // setting change for EmailJS
+    const [nameEJS, setName] = useState('');
+    const [emailEJS, setEmail] = useState('');
+    const [messageEJS, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+  // function to submit to EmailJS
+    const submit = () => {
+      if (nameEJS && emailEJS && messageEJS) {
+        const serviceId = process.env.REACT_APP_SERVICE_ID;
+        const templateId = process.env.REACT_APP_TEMPLATE_ID;
+        const userId = process.env.REACT_APP_USER_ID;
+
+        const templateParams = {
+            nameEJS,
+            emailEJS,
+            messageEJS
+        };
+        emailjs.send(serviceId, templateId, templateParams, userId)
+            .then(response => console.log(response))
+            .then(error => console.log(error));
+  
+          setName('');
+          setEmail('');
+          setMessage('');
+          setEmailSent(true);
+          console.log('Email sent!');
+      } else {
+          alert('Please fill in all fields.');
+      }
+  };
     
   return (
     <section>
@@ -42,17 +78,17 @@ function Contact() {
     <Form className='mx-5' onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicName">
         <Form.Label>Name:</Form.Label>
-        <Form.Control type="name" placeholder="Name" name='Name' defaultValue={name} onBlur={handleChange} />
+        <Form.Control type="name" placeholder="Name" name='Name' defaultValue={name} onBlur={handleChange} value={nameEJS} onChange={e => setName(e.target.value)} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" name='Email' placeholder="Enter email" defaultValue={email} onBlur={handleChange} />
+        <Form.Control type="email" name='Email' placeholder="Enter email" defaultValue={email} onBlur={handleChange} value={emailEJS} onChange={e => setEmail(e.target.value)} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Message:</Form.Label>
-        <Form.Control as="textarea" name="Message" rows={5} defaultValue={message} onBlur={handleChange}/>
+        <Form.Control as="textarea" name="Message" rows={5} defaultValue={message} onBlur={handleChange} value={messageEJS} onChange={e => setMessage(e.target.value)}/>
       </Form.Group>
 
       {errorMessage && (
@@ -61,9 +97,10 @@ function Contact() {
                 </div>
               )}
 
-      <Button variant="dark" type="submit">
+      <Button variant="dark" type="submit" onClick={submit}>
         Submit
       </Button>
+      <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
     </Form>
     </section>
   );
